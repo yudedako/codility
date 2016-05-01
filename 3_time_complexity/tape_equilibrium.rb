0a09bd22e2524cab4dbadb0a3803cce9c9b40a77
yudedako <pluto.club+git@gmail.com>
2016-05-01 22:29:48 +0900

    fix/3_time_complexity/tape_equilibrium

--- 3_time_complexity/tape_equilibrium.rb
+++ 3_time_complexity/tape_equilibrium.rb
@@ -3,7 +3,9 @@ require 'byebug'
 require 'pry-byebug'
 require 'minitest/autorun'
 
-# Test score 41%
+# Test score 83%
+# Correctness: 66%
+# Performance: 100%
 
 # Correctness tests
 
@@ -11,43 +13,19 @@ require 'minitest/autorun'
 # two elements ✘WRONG ANSWER
 # got 0 expected 2000
 
-# simple_negative
-# simple test with negative numbers, length = 5 ✘WRONG ANSWER
-# got -27 expected 3
-
-# small_random
-# random small, length = 100 ✘WRONG ANSWER
-# got -12771 expected 39
-
 # small
 # small elements ✘WRONG ANSWER
 # got 0 expected 20
-
-
-# Performance tests
-
-# medium_random2
-# random medium, numbers from -1,000 to 50, length = ~10,000 ✘WRONG ANSWER
-# got -4731154 expected 196
-
-# large_random
-# random large, length = ~100,000 ✘WRONG ANSWER
-# got -156854 expected 2
-
-# large_extreme
-# large test with maximal and minimal values, length = ~100,000 ✘WRONG ANSWER 
-# got -1000 expected 1000
 def solution(a)
   total_sum = a.reduce(:+)
+  result, tmp_l, tmp_r = total_sum.abs, 0, total_sum
 
-  tmp = 0
-  result = total_sum
-  (0..a.length-2).each do |idx|
-    tmp += a[idx]
-    tmp2 = (tmp - (total_sum - tmp)).abs
-    return 0 if tmp2.zero?
+  (1..a.size-1).each do |idx|
+    tmp_l += a[idx-1]
+    tmp_r -= a[idx-1]
 
-    result = tmp2 if result > tmp2
+    tmp = (tmp_l-tmp_r).abs
+    result = tmp if result > tmp
   end
 
   return result
@@ -68,5 +46,21 @@ class Tests < MiniTest::Unit::TestCase
 
   def test_3
     assert_equal 4, solution([1,2,3,4,3,2,1])
+  end
+
+  def test_4
+    assert_equal 0, solution([-1000,1000])
+  end
+
+  def test_5
+    assert_equal 1, solution([-3,-1,-2,-4,-3])
+  end
+
+  def test_6
+    assert_equal 0, solution([10,10])
+  end
+
+  def test_7
+    assert_equal 0, solution([-10,-10])
   end
 end
