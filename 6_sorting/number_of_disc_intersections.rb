d52065cf4f3b1370b2d3775bb787ff9e3fc8aa76
yudedako <pluto.club+git@gmail.com>
2016-05-08 22:39:49 +0900

    fix/6_sorting/number_of_disc_intersections

--- 6_sorting/number_of_disc_intersections.rb
+++ 6_sorting/number_of_disc_intersections.rb
@@ -7,19 +7,21 @@ require 'minitest/autorun'
 # - Correctness 100%
 # - Performance 25%
 
+# パフォーマンスが達成できない。。。保留中
+
 ## Correctness tests
 
 ## Performance tests
 
 # medium3 ✘TIMEOUT ERROR
-# running time: 1.28 sec., time limit: 0.35 sec.
+# running time: 1.10 sec., time limit: 0.37 sec.
 
 # medium4 ✘TIMEOUT ERROR
-# running time: 4.84 sec., time limit: 0.38 sec.
+# running time: 4.16 sec., time limit: 0.39 sec.
 
 # 10M_intersections
 # 10.000.000 intersections ✘TIMEOUT ERROR
-# running time: 4.38 sec., time limit: 0.39 sec.
+# running time: 3.85 sec., time limit: 0.39 sec.
 
 # big1 ✘TIMEOUT ERROR
 # running time: >6.00 sec., time limit: 0.50 sec.
@@ -27,26 +29,16 @@ require 'minitest/autorun'
 # big2 ✘TIMEOUT ERROR
 # running time: >6.00 sec., time limit: 0.54 sec.
 
-# big3 
+# big3
 # [0]*50000 ✘TIMEOUT ERROR
 # running time: >6.00 sec., time limit: 0.55 sec.
 
 def solution(a)
   result = 0
-  a[0..a.size-1].each_with_index do |val, idx|
-    base_range_min = idx - val
-    base_range_max = idx + val
-    sub_idx = idx
-    a[idx+1..a.size].each do |sub_val|
-      sub_idx += 1
-      sub_range_min = sub_idx - sub_val
-      sub_range_max = sub_idx + sub_val
-
-      next if base_range_max < sub_range_min
-      next if base_range_min > sub_range_max
-
-      result += 1
-    end
+  a_size = a.size - 1
+  a[0..a_size].each_with_index do |val, idx|
+    result += (idx+1..a_size).count{|sub_idx| (idx + val) >= (sub_idx - a[sub_idx]) }
+    return -1 if result > 10_000_000
   end
   return result
 end
@@ -62,5 +54,9 @@ class Tests < MiniTest::Unit::TestCase
 
   def test_2
     assert_equal 0, solution([0,0])
+  end
+
+  def test_3
+    assert_equal 19997, solution(Array.new(10_000, 1))
   end
 end
