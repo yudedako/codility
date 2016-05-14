46104bc66946de2360a6c5e638cf069f7740bb4f
yudedako <pluto.club+git@gmail.com>
2016-05-14 23:08:26 +0900

    fix/7_stacks_and_queues/fish

--- 7_stacks_and_queues/fish.rb
+++ 7_stacks_and_queues/fish.rb
@@ -3,15 +3,11 @@ require 'byebug'
 require 'pry-byebug'
 require 'minitest/autorun'
 
-# Test score 37%
-# - Correctness 50%
+# Test score 50%
+# - Correctness 75%
 # - Performance 25%
 
 ## Correctness tests
-
-# extreme_small
-# 1 or 2 fishes ✘WRONG ANSWER
-# got 2 expected 1
 
 # simple2
 # simple test ✘WRONG ANSWER
@@ -22,7 +18,7 @@ require 'minitest/autorun'
 # medium_random
 # small medium test, N = ~5,000 ✘WRONG ANSWER
 # got 2498 expected 106
- 
+
 # large_random
 # large random test, N = ~100,000 ✘WRONG ANSWER
 # got 49859 expected 572
@@ -32,34 +28,54 @@ require 'minitest/autorun'
 # got 99999 expected 1
 
 def solution(a, b)
-  live_up_fish, live_down_fish = 0, 0
-  up_fish, down_fish = nil, nil
+  live_up_fish = 0
+  stack_fishs = []
 
-  0.upto(a.size-1) do |idx|
-    if b[idx].zero?
-      up_fish = a[idx] if idx > 0
-      live_up_fish += 1
-    else
-      down_fish = a[idx] if idx > 0
-      live_down_fish += 1
+  a.each_with_index do |size, idx|
+    if b[idx].to_i == 1
+      stack_fishs << size
+      next
     end
 
-    next unless up_fish && down_fish
-
-    if up_fish > down_fish
-      live_down_fish -= 1
-      down_fish = nil
-    else
-      live_up_fish -= 1
-      up_fish = nil
+    if stack_fishs.size > 0
+      if size < stack_fishs.last
+        next
+      else
+        stack_fishs.pop
+      end
     end
+    live_up_fish += 1
   end
 
-  return live_up_fish + live_down_fish
+  return live_up_fish + stack_fishs.size
 end
 
 class Tests < MiniTest::Unit::TestCase
   def test_basic
     assert_equal 2, solution([4,3,2,1,5], [0,1,0,0,0])
+  end
+
+  def test_1
+    assert_equal 1, solution([1], [0])
+  end
+
+  def test_2
+    assert_equal 1, solution([1], [1])
+  end
+
+  def test_3
+    assert_equal 2, solution([1, 2], [0, 0])
+  end
+
+  def test_4
+    assert_equal 2, solution([1, 2], [1, 1])
+  end
+
+  def test_5
+    assert_equal 1, solution([1, 2], [1, 0])
+  end
+
+  def test_6
+    assert_equal 2, solution([1, 2], [0, 1])
   end
 end
