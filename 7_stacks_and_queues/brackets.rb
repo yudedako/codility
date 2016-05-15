81f4be79b5c4f890c20424e179727002d6ad9794
yudedako <pluto.club+git@gmail.com>
2016-05-15 21:29:00 +0900

    fix/7_stacks_and_queues/brackets

--- 7_stacks_and_queues/brackets.rb
+++ 7_stacks_and_queues/brackets.rb
@@ -3,39 +3,25 @@ require 'byebug'
 require 'pry-byebug'
 require 'minitest/autorun'
 
-# Test score 62%
-# - Correctness 33%
-# - Performance 80%
+# Test score 100%
+# - Correctness 100%
+# - Performance 100%
 
 ## Correctness tests
 
-# negative_match
-# invalid structures ✘WRONG ANSWER
-# got 1 expected 0
-
-# simple_grouped
-# simple grouped positive and negative test, length=22 ✘WRONG ANSWER
-# got 1 expected 0
-
 ## Performance tests
-
-# large2 
-# simple large negative test, 10K+1 ('s followed by 10K )'s + )( + () ✘WRONG ANSWER
-# got 1 expected 0
 
 def solution(s)
   nests_pattern = { '(' => ')', '{' => '}', '[' => ']' }
   before_nests = []
-
   s.chars.each do |char|
     if nests_pattern.keys.include?(char)
       before_nests << char
-    elsif nests_pattern.values.include?(char)
-      return 0 if nests_pattern[before_nests.last] != char
-      before_nests.pop
+    else
+      return 0 unless nests_pattern[before_nests.pop] == char
     end
   end
-  return 1
+  return before_nests.size > 0 ? 0 : 1
 end
 
 class Tests < MiniTest::Unit::TestCase
@@ -45,5 +31,13 @@ class Tests < MiniTest::Unit::TestCase
 
   def test_basic_2
     assert_equal 0, solution('([)()]')
+  end
+
+  def test_1
+    assert_equal 1, solution('{}')
+  end
+
+  def test_2
+    assert_equal 0, solution('{')
   end
 end
