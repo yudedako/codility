cfcb5121559d1a195a1266254d548b55eba53f91
yudedako <pluto.club+git@gmail.com>
2016-05-16 23:55:23 +0900

    fix/7_stacks_and_queues/stone_wall

--- 7_stacks_and_queues/stone_wall.rb
+++ 7_stacks_and_queues/stone_wall.rb
@@ -3,67 +3,39 @@ require 'byebug'
 require 'pry-byebug'
 require 'minitest/autorun'
 
-# Test score 42%
-# - Correctness 60%
-# - Performance 33%
+# Test score 100%
+# - Correctness 100%
+# - Performance 100%
 
 ## Correctness tests
 
-# simple2 ✘WRONG ANSWER
-# got 4 expected 5
-
-# simple3 ✘WRONG ANSWER
-# got 9 expected 8
-
 ## Performance tests
 
-# medium1 ✘WRONG ANSWER
-# got 122 expected 117
-
-# medium2 ✘WRONG ANSWER
-# got 380 expected 357
-
-# medium3 ✘WRONG ANSWER
-# got 422 expected 403
-
-# medium4 ✘WRONG ANSWER
-# got 479 expected 468
-
-# large_up_to_20 ✘WRONG ANSWER
-# got 85934 expected 81986
-
-# large_up_to_100 ✘WRONG ANSWER 
-# got 96791 expected 94813
-
 def solution(h)
-  box_count = 0
-  before_size = nil
-  min_size_array = []
+  box_count, before_size, min_size_array = 0, nil, []
   h.each do |height|
     next if before_size == height
+    before_size = height
+
     if min_size_array.size > 0
-      if min_size_array.last == height
-        next
-      elsif min_size_array.last > height
+      next if min_size_array.last == height
+
+      if min_size_array.last > height
         next_flag = false
         while min_size_array.last > height
-          before_size = min_size_array.pop
+          min_size_array.pop
+          break if min_size_array.size.zero?
           if min_size_array.last == height
             next_flag = true
             break
           end
-          break if min_size_array.size.zero?
         end
         next if next_flag
-      else
-        min_size_array << height
       end
-    else
-      min_size_array << height
     end
 
+    min_size_array << height
     box_count += 1
-    before_size = height
   end
 
   return box_count
@@ -88,5 +60,13 @@ class Tests < MiniTest::Unit::TestCase
 
   def test_4
     assert_equal 2, solution([1,2,1])
+  end
+
+  def test_5
+    assert_equal 4, solution([1,2,3,2,3,2,1])
+  end
+
+  def test_6
+    assert_equal 3, solution([2,1,2,1])
   end
 end
